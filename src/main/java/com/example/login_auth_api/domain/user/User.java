5418ef08daa0d181c02beb8612e473e15543a436
Ricardo Vasconcelos <ricardovasc@gmail.com>
2024-08-06 12:19:18 -0300

    Add initial auth code and config

---
+++ src/main/java/com/example/login_auth_api/domain/user/User.java
@@ -0,0 +1,21 @@
+package com.example.login_auth_api.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+}
